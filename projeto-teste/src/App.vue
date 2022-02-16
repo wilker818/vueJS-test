3c4e74300061ddf9c72a8fa5bd81c12992b8eb78
wilker818 <wilkerferreira818@gmail.com>
2022-02-15 22:56:43 -0300

    New project
    Novo projeto criado e melhorado.

---
+++ projeto-teste/src/App.vue
@@ -0,0 +1,31 @@
+<template>   
+  <BaseComponentes/>  
+</template>
+
+<script>
+import BaseComponentes from './baseview/BaseComponentes.vue'
+
+
+export default {
+  name: 'App',
+  components: {    
+    BaseComponentes,    
+  }
+}
+</script>
+
+<style>
+html{
+ background: rgb(1,27,40);  
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #fff;
+  margin: 0px;
+  padding: 0px;  
+  background: rgb(1,27,40);   
+}
+</style>
